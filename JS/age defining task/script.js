1f6d0961ca80ac27391691bf663042a7c3488de1
hinnurainen <72872684+hinnurainen@users.noreply.github.com>
2021-09-07 13:20:25 +0300

    Delete JS directory

--- JS/age defining task/script.js
+++
@@ -1,6 +0,0 @@
-let age = prompt("How old are you?");
-if (age < 18) {
-    alert("You are too young, sorry.")
-} else {
-    alert("You are more than welcome!")
-}
--- JS/calculator task/script.js
+++
@@ -1,4 +0,0 @@
-let a = prompt("Enter the first number.");
-let b = prompt("Enter the second number.");
-
-alert("The sum for those numbers is " + (Number(a)+Number(b)))
